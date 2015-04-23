2b6bbff68d53d77cb29bb51385412542f39a7347
ewoods2011 <ebw3559@louisiana.edu>
2015-04-23 17:18:21 -0500

    Finally got the cutscene in Level 1 working - Edward

---
+++ John and John/Assets/Edward Stuff/Scripts/ActivateJohn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateJohn : MonoBehaviour {
+
+    public GameObject John;
+    public GameObject JohnClone;
+    public GameObject Camera;
+
+	// Use this for initialization
+	void Start () {
+        John.SetActive(true);
+        JohnClone.SetActive(true);
+        Camera.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
