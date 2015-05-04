60af20b939d53f765bae832832146ab9eb752869
rbroussard54 <rbroussard5414@gmail.com>
2015-05-04 17:51:20 -0500

    Character swap timer script
    Got the script working. Just need to do the GUI part of it.

--- John and John/Assets/Becca's Scripts/SwitchTimer.cs
+++ John and John/Assets/Becca's Scripts/SwitchTimer.cs
@@ -2,6 +2,13 @@
 using System.Collections;
 
 public class SwitchTimer : MonoBehaviour {
+	
+	// Declare needed variables
+	private Vector3 tempPosition;
+	private Quaternion tempRotation;
+	private Vector3 cloneEulerAngles; // added to avoid rotating the clone oddly
+	public GameObject player;
+	public GameObject clone;
 
 	public float SwitchTime;
 	public GUISkin customSkin;
@@ -12,12 +19,16 @@ public class SwitchTimer : MonoBehaviour {
 	float blah;
 	float meh;
 	float seconds;
+
 	void Start()
 	{
-		//StartCoroutine("MyEvent");
 		GUI.skin = customSkin;
-		//SwitchTime = curTime;
 		blah = Time.time;
+	}
+
+	void Awake() {
+		QualitySettings.vSyncCount = 1;
+		Camera.main.transparencySortMode = TransparencySortMode.Orthographic;
 	}
 
 	void Update()
@@ -30,11 +41,30 @@ public class SwitchTimer : MonoBehaviour {
 		meh = Time.time - blah;
 
 		seconds = (meh % 60f);
-		curTime = (int)(SwitchTime - seconds);
-		//timeElapse -= Time.deltaTime;
-		//curTime = (int)timeElapse;
+		curTime = (int)((SwitchTime + 1) - seconds);
+
 		//print to GUI
+		if (curTime == 0)
+		{
+			Swap ();
+			blah = Time.time;
+		}
+
 		GUI.Label(new Rect(400f, 25f, 100f, 30f), curTime.ToString());
+	}
+
+	private void Swap() {
 		
+		tempPosition = player.transform.position;
+		player.transform.position = clone.transform.position;
+		clone.transform.position = tempPosition;
+		
+		var cam = FindObjectOfType<vp_FPCamera>();
+		
+		var tempAngles = cloneEulerAngles;
+		tempRotation = player.transform.rotation;
+		cloneEulerAngles = cam.transform.eulerAngles;
+		FindObjectOfType<vp_FPCamera>().SetRotation (tempAngles);
+		clone.transform.rotation = tempRotation;
 	}
 }
