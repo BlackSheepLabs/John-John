7a2aa6358ac244621f6ebfe2768b2c89c2bbb688
ewoods2011 <ebw3559@louisiana.edu>
2015-05-04 20:12:56 -0500

    Level 4 almost done
    Conflicts:
    	John and John/John and John-csharp.sln
    	John and John/John and John.sln

--- John and John/Assets/Edward Stuff/Scripts/ActivatePlatformOnTrigger.cs
+++ John and John/Assets/Edward Stuff/Scripts/ActivatePlatformOnTrigger.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class ActivatePlatformOnTrigger : Trigger {
 
 	public List<GameObject> movingPlatforms;
+	public List<vp_MovingPlatform> vp_Plats;
 
 	public bool IsActive
 	{get {return this.currentState == State.Activated;} }
@@ -16,19 +17,24 @@ public class ActivatePlatformOnTrigger : Trigger {
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	public override void Activate ()
 	{
 		foreach(GameObject plat in movingPlatforms)
 			plat.GetComponent<vp_MovingPlatform>().enabled = true;
+
+		foreach(vp_MovingPlatform plat in vp_Plats)
+			plat.SendMessage("GoTo", plat.TargetedWaypoint == 1, SendMessageOptions.DontRequireReceiver);
 	}
 	
 	public override void Deactivate ()
 	{
 		foreach(GameObject plat in movingPlatforms)
 			plat.GetComponent<vp_MovingPlatform>().enabled = false;
+
+		foreach(vp_MovingPlatform plat in vp_Plats)
+			plat.SendMessage("GoTo", plat.TargetedWaypoint == 0, SendMessageOptions.DontRequireReceiver);
 	}
 
 }
