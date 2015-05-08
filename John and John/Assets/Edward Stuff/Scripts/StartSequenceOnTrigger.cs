71e3ceeea704699857967b11de5baaf48ee83b59
ewoods2011 <ebw3559@louisiana.edu>
2015-05-08 14:16:09 -0500

    this sucks

---
+++ John and John/Assets/Edward Stuff/Scripts/StartSequenceOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSequenceOnTrigger : MonoBehaviour {
+
+	public GameObject AutoStartSequence;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider C)
+	{
+		if(AutoStartSequence != null)
+			AutoStartSequence.SetActive(true);
+
+	}
+}
---
+++ John and John/Assets/Scripts/Level7KillThePlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level7KillThePlayer : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider col) {
+		Debug.Log("WTF");
+		if (col.collider.tag == "Player") {
+		player.GetComponent<vp_FPPlayerDamageHandler>().Die();
+		AutoFade.LoadLevel(Application.loadedLevel + 1, 2f, 2f, Color.black);
+
+		}
+	}
+}
