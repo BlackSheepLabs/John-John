ddbd8e58ebe960481ccdd72077102683ef2907ae
rbroussard54 <rbroussard5414@gmail.com>
2015-05-08 11:47:32 -0500

    Please

---
+++ John and John/Assets/PlaayMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaayMusic : MonoBehaviour {
+
+	public GameObject Music;
+	// Use this for initialization
+	void Start () {
+
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (this.audio.isPlaying == false) {
+			Music.SetActive(true);
+		}
+	}
+}
